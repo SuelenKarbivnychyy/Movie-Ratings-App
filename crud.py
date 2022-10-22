a1e7d345fb08f8b12f8c7afb7da73f1490ce5c35
suelenk <suelenmatosr@gmail.com>
2022-10-22 16:03:35 -0400

    started part 3. created all_movies, homepage, movie_detals templates

--- crud.py
+++ crud.py
@@ -17,6 +17,16 @@ def create_movie(title, overview, release_date, poster_path):
 
     return movie
 
+def show_movies():
+    """Shows all movies."""
+
+    return Movie.query.all()
+
+def get_movie_by_id(movie_id):
+    """Get a movie by its id."""
+
+    return Movie.query.get(movie_id)
+
 def create_rating(user, movie, score):
     """Create and return a new rating."""
 
